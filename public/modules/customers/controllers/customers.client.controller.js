143bd63fbd453398ffcb126c934461255f572b54
taylsonmartinez <taylson.martinez@gmail.com>
2015-04-14 18:19:14 -0300

    Modal Styling & AngularJS Form Validation

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -4,14 +4,51 @@
 
 var customersApp = angular.module('customers');
 
-customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authentication', 'Customers',
-	function($scope, $stateParams, Authentication, Customers) {
+customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authentication', 'Customers', '$modal', '$log',
+	function($scope, $stateParams, Authentication, Customers, $modal, $log) {
 
 		this.authentication = Authentication;
 
 
 		// Find a list of Customers
 		this.customers = Customers.query();
+
+		//open a modal window to Update a single customer record
+		this.modalUpdate = function (size, selectedCustomer) {
+
+			var modalInstance = $modal.open({
+				templateUrl: 'modules/customers/views/edit-customer.client.view.html',
+				controller: function ($scope, $modalInstance, customer) {
+					$scope.customer = customer;
+
+					$scope.ok = function () {
+
+						if(updateCustomerForm.$valid){
+							$modalInstance.close($scope.customer);
+						}
+
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
+				},
+				size: size,
+				resolve: {
+					customer: function  () {
+						return selectedCustomer;
+					}
+				}
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+				$scope.selected = selectedItem;
+
+
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
 
 	}
 ]);
@@ -22,8 +59,19 @@ customersApp.controller('CustomersCreateController', ['$scope', 'Customers',
 	}
 ]);
 
-customersApp.controller('CustomersEditController', ['$scope', 'Customers',
+customersApp.controller('CustomersUpdateController', ['$scope', 'Customers',
 	function($scope, Customers) {
+
+		// Update existing Customer
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
+
+			customer.$update(function() {
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
 
 	}
 ]);
